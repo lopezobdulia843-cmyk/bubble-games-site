4927dbf9ceb5bbc31c8e62496687dd9e03276659
lopezobdulia843-cmyk <lopezobdulia843@gmail.com>
2026-04-25 15:45:05 -0700

    Update supabase.js

--- BubbleGames/static/supabase.js
+++ BubbleGames/static/supabase.js
@@ -2,7 +2,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 // Use the exact URL and Key we found earlier
-const supabaseUrl = 'https://ygpknejcukucsygokaen.supabase.co';
+const supabaseUrl = 'https://bubblegames.onrender.com/supabase';
 const supabaseKey = 'sb_publishable_5JbFhSI73LX6H2oehrtizA_zu-tWKjc';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
